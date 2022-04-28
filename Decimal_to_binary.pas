ea7b292af52d0a60c817c75fad5b2fef97aada05
Nguyễn Anh Sáng <sn15062008@gmail.com>
2022-04-28 22:03:54 +0700

    Update Decimal_to_binary.pas

--- Decimal_to_binary.pas
+++ Decimal_to_binary.pas
@@ -1,5 +1,5 @@
 uses crt,sysutils;
 var i,n:longint;
 begin
-//DON'T USE THIS FOR NOW CUZ IT'S BUGGY :) I will fix it later !!!
+//DON'T USE THIS FOR NOW CUZ IT'S BUGGY :) I will rewrite and fix as soon as I can !!!
 end;
