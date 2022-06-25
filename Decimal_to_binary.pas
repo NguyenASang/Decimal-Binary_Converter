582b1d1472381230c19446f7aca55a2d5cd0e2e9
Nguyễn Anh Sáng <sn15062008@gmail.com>
2022-06-25 21:19:39 +0700

    Changes:
    - Add option to display or hide forever loop part 
    - Fix issue when delete input
    - Display result in txt file if it is too long to display in console

--- Decimal_to_binary.pas
+++ Decimal_to_binary.pas
@@ -1,14 +1,15 @@
 uses crt,sysutils;
 var i,sep,cnt:longint;
-    s,num_bin,num_div,num_res,dec_mul,dec_res,compare:ansistring;
-    decimal,remem,negative,loop:boolean;
+    s,num_bin,num_div,num_res,dec_mul,dec_res,dec_out,compare:ansistring;
+    decimal,remem,negative,loop,show_loop,pass:boolean;
     key:char;
+    file_out:text;
 
 procedure Decimal_part;
 begin
 write('.');
 
-dec_res:=''; dec_mul:=''; compare:='';
+dec_res:=''; dec_mul:=''; compare:=''; dec_out:='';
 for i:=sep - 1 to length(s) do
   begin
   if i < sep then dec_res:=concat('0',dec_res) else dec_res:=concat(dec_res,s[i]);
@@ -54,47 +55,131 @@ repeat
 
     if i = 1 then
       begin
-      if (compare = dec_res) and (cnt <> length(s) - sep + 1) then
+      if (compare = dec_res) and (cnt <> length(s) - sep + 1) and (show_loop = true) or (cnt = 100) and (show_loop = false) then
         begin
         loop:=true;
 
-        TextColor(White);
-        writeln('...');
+        if length(dec_out) + length(num_res) + 1 >= 2550 then
+          begin
+          writeln(file_out,')...');
+          end
 
-        TextColor(Red);
-        write(sLineBreak,'Note: ');
+        else begin
+          TextColor(White);
+          writeln('...');
+          end;
 
-        TextColor(Green);
-        write('Green part');
+        if (show_loop = true) and (length(dec_out) + length(num_res) + 1 < 2550) then
+          begin
+          TextColor(Red);
+          write(sLineBreak,'Note: ');
 
-        TextColor(White);
-        write(' is the forever loop part');
+          TextColor(Green);
+          write('Green part');
+
+          TextColor(White);
+          write(' is the forever loop part');
+          end;
 
         break;
         end;
 
       if cnt = length(s) - sep + 1 then
         begin
+        repeat
+          pass:=false;
+
+          clrscr;
+          writeln('Enter decimal to convert: ',s);
+
+          writeln(slineBreak,'Convert to decimal: ',sLineBreak,num_res,'.',dec_out);
+
+          write(sLineBreak, 'Do you want to show looping part ?');
+
+          writeln(' Y = Yes | N = No | L = Learn more');
+
+          case readkey of
+            'y', 'Y' : begin
+                       show_loop:=true;
+                       pass:=true;
+                       end;
+
+            'n', 'N' : begin
+                       show_loop:=false;
+                       pass:=true;
+                       end;
+
+            'l', 'L' : begin
+                       clrscr;
+                       writeln('Enter decimal to convert: ',s);
+
+                       writeln(slineBreak,'Convert to decimal: ',sLineBreak,num_res,'.',dec_out);
+
+                       TextColor(Yellow);
+                       writeln(sLineBreak, 'Learn more: ');
+
+                       TextColor(White);
+                       writeln('- Showing the loop part gives you more accurate results, but sometimes takes long time to display.');
+                       writeln('- If you do not want to display looping part, it will only display first 100 characters of the result.');
+                       readln;
+                       end;
+          end;
+        until pass = true;
+
+        clrscr;
+        writeln('Enter decimal to convert: ',s);
+
+        write(slineBreak,'Convert to decimal: ',sLineBreak,num_res,'.',dec_out);
+
+        if show_loop = true then TextColor(Green);
+
         compare:=dec_res;
-        TextColor(Green);
         end;
 
       if remem = true then
-          begin
-          dec_res[1]:='1';
-          write(1);
-          end;
-
-      if remem = false then
         begin
+        dec_res[1]:='1';
+        dec_out:=concat(dec_out,'1');
+        if length(dec_out) + length(num_res) + 1 > 2550 then write(file_out,1) else write(1);
+        end
+
+      else begin
         dec_res[1]:='0';
-        write(0);
+        dec_out:=concat(dec_out,'0');
+        if length(dec_out) + length(num_res) + 1 > 2550 then write(file_out,0) else write(0);
         end;
       end;
     end;
 
   inc(cnt);
+
+  if length(dec_out) + length(num_res) + 1 = 2550 then
+  begin
+  clrscr;
+
+  TextColor(Red);
+  write('Alert: ');
+
+  TextColor(White);
+  write('Output is in text file because the result was too long');
+
+  Insert('(',dec_out,length(s) - sep + 1);
+
+  assign(file_out,'output.txt'); rewrite(file_out);
+
+  write(file_out,'Convert to decimal: ',sLineBreak,num_res,'.',dec_out);
+  end;
 until (round(StrToFloat(dec_res)) - StrToFloat(dec_res) = 0) or (loop = true);
+
+if length(dec_out) + length(num_res) + 1 >= 2550 then
+  begin
+
+  write(file_out,sLineBreak,'Note: Numbers in bracket is the looping forever part');
+
+  close(file_out);
+
+  executeprocess('notepad.exe',['output.txt']);
+  end;
 end;
 
 procedure Integer_part;
@@ -181,13 +266,6 @@ GotoXY(27,1);
 //Note: 13 = Enter | 8 = Backspace
 
 repeat
-  if length(s) = 255 then
-    begin
-    clrscr;
-    GotoXY(1,1);
-    write('Enter decimal to convert: ',s);
-    end;
-
   key:=readkey;
 
   if (key in ['0'..'9']) or (key = '-') and (length(s) = 0) or (key = '.') and (length(s) > 0) and (decimal = false) then
@@ -236,7 +314,11 @@ repeat
     begin
     if negative  = true then delete(s,1,1);
 
-    if s[length(s)] = '.' then delete(s,length(s),1);
+    if s[length(s)] = '.' then
+      begin
+      decimal:=false;
+      delete(s,length(s),1);
+      end;
 
     if (length(s) > 1) and (decimal = true) and (s[length(s)] = '0') then
       begin
