e7bbe6125360c9c3f2ad9f61c8629cd8b3133514
Nguyễn Anh Sáng <sn15062008@gmail.com>
2022-08-16 10:11:25 +0700

    Well, that's a long time:)

--- Decimal_to_binary.pas
+++ Decimal_to_binary.pas
@@ -1,23 +1,145 @@
-uses crt,sysutils;
-var i,sep,cnt:longint;
-    s,num_bin,num_div,num_res,dec_mul,dec_res,dec_out,compare:ansistring;
-    decimal,remem,negative,loop,show_loop,pass:boolean;
-    key:char;
-    file_out:text;
+uses windows,sysutils,keyboard,strutils,regexpr;
+var s,num_bin,num_div,num_res,dec_mul,dec_res,compare,result,limit: ansistring;
+    decimal,remem,negative,loop,show_loop,pass,ctrl_c: boolean;
+    i,sep,cnt: longint;
+    pre_pos: coord;
+    key: char;
 
-procedure Decimal_part;
+Function WhereXY: coord;
+var cursor_pos: TConsoleScreenBufferInfo;
 begin
+GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), cursor_pos);
+WhereXY:=cursor_pos.dwCursorPosition;
+end;
+
+Procedure GoToXY(x,y: longint);
+var cursor_pos: coord;
+begin
+cursor_pos.x:=x;
+cursor_pos.y:=y;
+SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cursor_pos);
+end;
+
+Procedure TextColor(Color: Byte);
+begin
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), Color);
+end;
+
+Function Readkey: char;
+var k: TKeyEvent;
+begin
+InitKeyBoard;
+  k:=TranslateKeyEvent(GetKeyEvent);
+  readkey:=GetKeyEventChar(k);
+DoneKeyBoard;
+end;
+
+Function ScreenXY: coord;
+var ScreenSize: TConsoleScreenBufferInfo;
+begin
+GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), ScreenSize);
+ScreenXY:=ScreenSize.dwSize;
+end;
+
+Procedure Clear(start_x, start_y, area, end_x, end_y: longint);
+var dwNumWritten: dword;
+    pos: coord;
+begin
+pos.x:=start_x;
+pos.y:=start_y;
+FillConsoleOutputCharacter(GetStdHandle(STD_OUTPUT_HANDLE), ' ', area, pos, &dwNumWritten);
+GotoXY(end_x, end_y);
+end;
+
+Function Regex(str_match, reg: ansistring): boolean;
+var expr: TRegExpr;
+begin
+expr:=TRegExpr.Create;
+expr.Expression:=reg;
+
+if expr.Exec(str_match) then Regex:=true
+else Regex:=false;
+
+expr.Free;
+end;
+
+Function PasteFromClip: ansistring;
+var hClipData: handle;
+    StrData: pchar;
+begin
+OpenClipboard(0);
+  hClipdata:=GetClipboardData(CF_TEXT);
+  StrData:=GlobalLock(hClipData);
+  GlobalUnlock(hClipData);
+CloseClipboard;
+
+if (Regex(strpas(StrData),'^[-]?((\d+(\.\d*)?)|(\.\d+))$') = false) or (pos('.',strpas(strData)) <> 0) and (decimal = true) or (pos('-',strpas(strData)) <> 0) and (negative = true) then
+  begin
+  pre_pos:=WhereXY;
+
+  TextColor($4);
+  write(slinebreak,slinebreak,'Warning: ');
+
+  TextColor($7);
+  write('Your clipboard contains invalid characters');
+
+  repeat until readkey = #13;  // <- replace for readln
+
+  Clear(pre_pos.x, pre_pos.y, ScreenXY.x * 3, pre_pos.x, pre_pos.y);
+
+  PasteFromClip:='';
+  end
+
+else PasteFromClip:=Strpas(strdata);
+end;
+
+Procedure CopyToClip;
+var pchData,StrData: pchar;
+    hClipData: HGlobal;
+begin
+if decimal = true then result:=concat(num_res,'.',result) else result:=num_res;
+
+Openclipboard(0);
+  EmptyClipboard;
+
+  strData:=Stralloc(length(result) + 1);
+  StrPCopy(strData,result);
+
+  hClipData:=GlobalAlloc(GMEM_MOVEABLE,length(strData) + 1);
+  pchData:=GlobalLock(hClipData);
+  strcopy(pchData, LPCSTR(StrData));
+  GlobalUnlock(hClipData);
+
+  SetClipboardData(CF_TEXT,hclipData);
+CloseClipboard;
+
+Clear(0, WhereXY.y - 2, ScreenXY.x, 0, WhereXY.y - 2);
+
+TextColor($2);
+write('Status: ');
+
+TextColor($E);
+write('Copied');
+end;
+
+Function HandlerRoutine(dwCtrlType: DWORD): WINBOOL; stdcall;
+begin
+if (dwCtrlType = CTRL_C_EVENT) and (ctrl_c = true) then CopyToClip;
+end;
+
+Procedure Decimal_part;
+begin
+cnt:=0;
+loop:=false;
+dec_res:='0' + Copy(s,sep,length(s));
+
 write('.');
 
-dec_res:=''; dec_mul:=''; compare:=''; dec_out:='';
-for i:=sep - 1 to length(s) do
-  begin
-  if i < sep then dec_res:=concat('0',dec_res) else dec_res:=concat(dec_res,s[i]);
-  end;
-
-compare:=''; cnt:=1; loop:=false;
 repeat
-  dec_mul:=dec_res; dec_res:=''; remem:=false;
+  inc(cnt);
+  remem:=false;
+  dec_mul:=dec_res; dec_res:='';
+
   for i:=length(dec_mul) downto 1 do
     begin
     if (dec_mul[i] <> '.') and (i <> 1) then
@@ -55,141 +177,110 @@ repeat
 
     if i = 1 then
       begin
-      if (compare = dec_res) and (cnt <> length(s) - sep + 1) and (show_loop = true) or (cnt = 100) and (show_loop = false) then
+      if (compare = dec_res) and (cnt <> length(s) - sep + 1) then
         begin
-        loop:=true;
+        loop:=true
+        end;
 
-        if length(dec_out) + length(num_res) + 1 >= 2550 then
+      if (cnt = length(s) - sep + 1) and (show_loop = true) then
+        begin
+        compare:=dec_res;
+        Textcolor($2);
+        end;
+
+      if (loop = false) then
+        begin
+        if (remem = true) then
           begin
-          writeln(file_out,')...');
+          dec_res[1]:='1';
+          write(1);
+          result:=concat(result,'1');
           end
 
         else begin
-          TextColor(White);
-          writeln('...');
+          dec_res[1]:='0';
+          write(0);
+          result:=concat(result,'0');
           end;
-
-        if (show_loop = true) and (length(dec_out) + length(num_res) + 1 < 2550) then
-          begin
-          TextColor(Red);
-          write(sLineBreak,'Note: ');
-
-          TextColor(Green);
-          write('Green part');
-
-          TextColor(White);
-          write(' is the forever loop part');
-          end;
-
-        break;
         end;
 
-      if cnt = length(s) - sep + 1 then
+      if (show_loop = false) and (IntToStr(length(result)) = limit) then write('...');
+
+      if GetAsyncKeyState(27) < 0 then
         begin
+        repeat until readkey <> ''; // <- prevent auto exit this procedure
+
+        pre_pos:=WhereXY;
+
+        TextColor($7);
+        write('...');
+
+        TextColor($4);
+        write(slinebreak,slinebreak,'Warning: ');
+
+        TextColor($7);
+        write('The converter has been paused');
+
+        write(slinebreak,slinebreak,'Press any key to ');
+
+        TextColor($2);
+        write('continue ');
+
+        TextColor($7);
+        write('| Esc to ');
+
+        TextColor($4);
+        write('stop ');
+
+        TextColor($7);
+        write('the converter');
+
         repeat
-          pass:=false;
+          key:=readkey;
 
-          clrscr;
-          writeln('Enter decimal to convert: ',s);
+          // for some reasons pre_pos.y is broken here, I have no idea about this
+          if key = #27 then
+            begin
+            GotoXY(pre_pos.x + 3,WhereXY.y - 4);
+            Clear(WhereXY.x,WhereXY.y,ScreenXY.x * 5,WhereXY.x,WhereXY.y);
+            exit;
+            end
 
-          writeln(slineBreak,'Convert to decimal: ',sLineBreak,num_res,'.',dec_out);
-
-          write(sLineBreak, 'Do you want to show looping part ?');
-
-          writeln(' Y = Yes | N = No | L = Learn more');
-
-          case readkey of
-            'y', 'Y' : begin
-                       show_loop:=true;
-                       pass:=true;
-                       end;
-
-            'n', 'N' : begin
-                       show_loop:=false;
-                       pass:=true;
-                       end;
-
-            'l', 'L' : begin
-                       clrscr;
-                       writeln('Enter decimal to convert: ',s);
-
-                       writeln(slineBreak,'Convert to decimal: ',sLineBreak,num_res,'.',dec_out);
-
-                       TextColor(Yellow);
-                       writeln(sLineBreak, 'Learn more: ');
-
-                       TextColor(White);
-                       writeln('- Showing the loop part gives you more accurate results, but sometimes takes long time to display.');
-                       writeln('- If you do not want to display looping part, it will only display first 100 characters of the result.');
-                       readln;
-                       end;
-          end;
-        until pass = true;
-
-        clrscr;
-        writeln('Enter decimal to convert: ',s);
-
-        write(slineBreak,'Convert to decimal: ',sLineBreak,num_res,'.',dec_out);
-
-        if show_loop = true then TextColor(Green);
-
-        compare:=dec_res;
-        end;
-
-      if remem = true then
-        begin
-        dec_res[1]:='1';
-        dec_out:=concat(dec_out,'1');
-        if length(dec_out) + length(num_res) + 1 > 2550 then write(file_out,1) else write(1);
-        end
-
-      else begin
-        dec_res[1]:='0';
-        dec_out:=concat(dec_out,'0');
-        if length(dec_out) + length(num_res) + 1 > 2550 then write(file_out,0) else write(0);
+          else begin
+            GotoXY(pre_pos.x,WhereXY.y - 4);
+            Clear(WhereXY.x,WhereXY.y,ScreenXY.x * 5,WhereXY.x,WhereXY.y);
+            TextColor($2);
+            break;
+            end
+        until key <> '';
         end;
       end;
     end;
+until ('1.0' + dupestring('0',length(dec_res) - 3) = dec_res) or (loop = true) or (IntToStr(length(result)) = limit) and (show_loop = false);
 
-  inc(cnt);
-
-  if length(dec_out) + length(num_res) + 1 = 2550 then
+if loop = true then
   begin
-  clrscr;
+  TextColor($7);
+  writeln('...');
 
-  TextColor(Red);
-  write('Alert: ');
+  Textcolor($4);
+  write(sLineBreak,'Note: ');
 
-  TextColor(White);
-  write('Output is in text file because the result was too long');
+  Textcolor($2);
+  write('Green part');
 
-  Insert('(',dec_out,length(s) - sep + 1);
-
-  assign(file_out,'output.txt'); rewrite(file_out);
-
-  write(file_out,'Convert to decimal: ',sLineBreak,num_res,'.',dec_out);
-  end;
-until (round(StrToFloat(dec_res)) - StrToFloat(dec_res) = 0) or (loop = true);
-
-if length(dec_out) + length(num_res) + 1 >= 2550 then
-  begin
-
-  write(file_out,sLineBreak,'Note: Numbers in bracket is the looping forever part');
-
-  close(file_out);
-
-  executeprocess('notepad.exe',['output.txt']);
+  Textcolor($7);
+  write(' is the forever loop part');
   end;
 end;
 
-procedure Integer_part;
+Procedure Integer_part;
 begin
-num_bin:=''; num_res:='';
-for i:=1 to sep - 1 do num_bin:=concat(num_bin,s[i]);
+num_bin:=Copy(s, 1, sep - 1);
+num_res:=''; num_div:='';
 
-num_div:=s;
 repeat
-  if num_div <> s then num_bin:=num_div;
+  if num_div <> '' then num_bin:=num_div;
 
   num_div:='';
 
@@ -241,141 +332,195 @@ repeat
         end;
       end;
     end;
-until (length(num_div) = 1) and (num_div ='0');
+until (num_div = '0');
+
+if negative = true then num_res:=concat('-',num_res);
 
 write(num_res);
 
 if decimal = true then Decimal_part;
 end;
 
-procedure Input;
+Function Input(check_dec, check_neg: boolean): ansistring;
 begin
-clrscr;
-write('Enter decimal to convert: ');
-
-GotoXY(1,3);
-
-TextColor(yellow);
-write('Tip: ');
-
-TextColor(White);
-write('Ctrl + C = Copy | Right click = Paste');
-
-GotoXY(27,1);
-
-//Note: 13 = Enter | 8 = Backspace
+input:='';
 
 repeat
   key:=readkey;
 
-  if (key in ['0'..'9']) or (key = '-') and (length(s) = 0) or (key = '.') and (length(s) > 0) and (decimal = false) then
+  if (key in ['0'..'9']) and (GetAsyncKeyState(VK_CONTROL) >= 0) and (GetAsyncKeyState(86) >= 0) or (key = '-') and (length(input) = 0) and (check_neg = true) or (key = '.') and (length(input) > 0) and (decimal = false) and (check_dec = true) then
     begin
     if key = '-' then negative:=true;
 
     if key = '.' then
       begin
-      decimal:=true; sep:=length(s) + 1;
+      decimal:=true;
+      sep:=length(input) + 1;
       end;
 
-    s:=concat(s,key);
+    input:=concat(input,key);
 
     write(key);
     end;
 
-  if length(s) = 1 then
+  if (key = #22) or (GetAsyncKeyState(VK_CONTROL) < 0) and (GetAsyncKeyState(86) < 0) then
     begin
-    clrscr;
-    write('Enter decimal to convert: ',s);
+    i:=length(input);
+    input:=concat(input,PasteFromClip);
+
+    write(Copy(input, i + 1,length(input)));
+
+    if pos('.',input) <> 0 then decimal:=true;
+
+    if pos('-',input) <> 0 then negative:=true;
     end;
 
-  if (ord(key) = 8) and (length(s) <> 0) then
+  if (key = #8) and (length(input) <> 0) then
     begin
-    if s[length(s)] = '.' then decimal:=false;
+    if input[length(input)] = '.' then decimal:=false;
 
-    if s[length(s)] = '-' then negative:=false;
+    if input[length(input)] = '-' then negative:=false;
 
-    if WhereX = 1 then
+    if WhereXY.x = 0 then
       begin
-      GotoXY((length(s) + 26) div (WhereY - 1),WhereY - 1);
+      GotoXY(ScreenXY.x - 1,WhereXY.y - 1);
       write(' ');
-      GotoXY((length(s) + 26) div (WhereY - 1),WhereY - 1);
+      GotoXY(ScreenXY.x - 1,WhereXY.y);
       end
 
     else begin
-      GotoXY(WhereX - 1,WhereY);
+      GotoXY(WhereXY.x - 1,WhereXY.y);
       write(' ');
-      GotoXY(WhereX - 1,WhereY);
+      GotoXY(WhereXY.x - 1,WhereXY.y);
       end;
 
-    delete(s,length(s),1);
+    delete(input,length(input),1);
     end;
 
-  if ord(key) = 13 then
+  if (key = #13) and (length(input) <> 0) then
     begin
-    if negative  = true then delete(s,1,1);
+    if (negative = true) and (check_neg = true) then delete(input,1,1);
 
-    if s[length(s)] = '.' then
+    if input[length(input)] = '.' then
       begin
       decimal:=false;
-      delete(s,length(s),1);
+      delete(input,length(input),1);
       end;
 
-    if (length(s) > 1) and (decimal = true) and (s[length(s)] = '0') then
+    if input[1] = '.' then
       begin
-      i:=length(s) + 1;
+      input:=concat('0',input);
+      end;
+
+    if (length(input) > 1) and (decimal = true) and (input[length(input)] = '0') and (check_dec = true) then
+      begin
+      i:=length(input) + 1;
       repeat
         dec(i);
-        if s[i] = '.' then decimal:=false;
-        delete(s,i,1);
-      until s[i - 1] in ['1'..'9'];
+        if input[i] = '.' then decimal:=false;
+        delete(input,i,1);
+      until input[i - 1] in ['1'..'9'];
       end;
 
-    if (length(s)> 1) and (s[1] = '0') and (s[2] <> '.') then
+    if (length(input) > 1) and (input[1] = '0') and (input[2] <> '.') then
       begin
       i:=1;
       repeat
-        delete(s,i,1);
-      until (s[i] = '0') and (s[i + 1] = '.') or (s[i] in ['1'..'9']);
+        delete(input,i,1);
+      until (input[i] = '0') and (input[i + 1] = '.') or (input[i] in ['1'..'9']);
       end;
 
-    if decimal = false then sep:=length(s) + 1
+    if decimal = false then sep:=length(input) + 1
 
     else begin
-      for i:=1 to length(s) do
+      for i:=1 to length(input) do
         begin
-        if s[i] = '.' then sep:=i;
+        if input[i] = '.' then sep:=i;
         end;
       end;
-
-    writeln(sLineBreak,slineBreak,'Convert to decimal: ');
     end;
-until ord(key) = 13;
+until (key = #13) and (length(input) <> 0);
 
-Integer_part;
+if (decimal = true) and (check_dec = true) then
+  begin
+  write(slinebreak,sLineBreak, 'Do you want to show looping part of the decimal part ?');
+
+  writeln(' Y = Yes | N = No');
+
+  TextColor($4);
+  write(slinebreak,'Warning: ');
+
+  TextColor($7);
+  writeln('Choose "Yes" will provide more precision result but sometimes it cans take some times to calculate.');
+  write('         Choose "No" will provide to you an option to choose how many digits do you want to display.');
+
+  repeat
+    case lowercase(readkey) of
+      'y' : begin
+            clear(0, WhereXY.y - 3, ScreenXY.x * 4, 0, WhereXY.y - 5);
+
+            show_loop:=true; pass:=true;
+            end;
+
+      'n' : begin
+            clear(0, WhereXY.y - 3, ScreenXY.x * 4, 0, WhereXY.y - 4);
+
+            pre_pos:=WhereXY;
+
+            write(slinebreak,'Enter how many digits do you want to display: ');
+
+            limit:=Input(false,false);
+
+            clear(0, pre_pos.y, ScreenXY.x * (ScreenXY.y - pre_pos.y), 0, pre_pos.y - 1);
+
+            show_loop:=false; pass:=true;
+            end;
+    end;
+  until pass = true;
+  end;
 end;
 
 begin
 repeat
-  Input;
+  ctrl_c:=false;
+  SetconsoleCtrlHandler(@handlerRoutine, TRUE);
+
+  clear(0, 0, ScreenXY.x * ScreenXY.y, 0, 0);
+  write('Enter decimal to convert: ');
+
+  s:=Input(true,true);
+
+  writeln(sLineBreak,slineBreak,'Convert to decimal: ');
+
+  Integer_part;
+
+  ctrl_c:=true;
+
+  TextColor($E);
+  write(slinebreak,slinebreak,'Tip: ');
+
+  TextColor($7);
+  write('You can copy the result by pressing Ctrl + C');
 
   write(sLineBreak,sLineBreak,'Press any key to ');
 
-  TextColor(Green);
+  TextColor($2);
   write('back');
 
-  TextColor(White);
+  Textcolor($7);
   write(' | Esc to ');
 
-  TextColor(Red);
+  Textcolor($4);
   write('exit');
 
-  TextColor(White);
+  repeat
+    key:=readkey;
+    if key = #3 then CopyToClip else break;
+  until key = #27;
 
-  decimal:=false; negative:=false; loop:=false;
-  s:='';
-until ord(readkey) = 27;
+  Textcolor($7);
 
-exit;
-
-readln;
+  result:='';
+  decimal:=false; negative:=false;
+until key = #27;
 end.
