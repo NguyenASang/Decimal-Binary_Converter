1775fea1271e77c2450c5912e190b270f3ff937f
Nguyễn Anh Sáng <sn15062008@gmail.com>
2022-09-17 21:01:39 +0700

    Update Binary_to_decimal.pas

--- Binary_to_decimal.pas
+++ Binary_to_decimal.pas
@@ -5,7 +5,7 @@ const Green       = $2;
       Red         = $4;
       White       = $7;
 
-var s, num_mul, num_res, dividend, div_res, dec_plus, dec_sum, dec_res: ansistring;
+var s, num_mul, num_res, dividend, div_res, dec_sum, dec_res: ansistring;
     ctrl_c, decimal, negative, remem: boolean;
     i, u, sep: longint;
     pre_pos: coord;
@@ -138,146 +138,107 @@ end;
 
 Procedure Decimal_part;
 begin
-div_res:=''; dec_res:=''; dec_sum:='0.0';
+dec_sum:='0.0'; div_res:='1.0';
 
 for i:=sep + 1 to length(s) do
   begin
-  if (length(div_res) = 0) then dividend:='1'
-
-  else begin
-    dividend:=div_res; div_res:='';
-    end;
-
+  u:=0;
   remem:=false;
-  for u:=1 to length(dividend) + 1 do
-    begin
-    if (dividend[u] <> '.') then
+  dividend:=div_res; div_res:='';
+
+  repeat
+    inc(u);
+
+    if (dividend[u] = '.') then div_res:=div_res + '.'
+
+    else if (StrToInt(dividend[u]) mod 2 = 1) then
       begin
-      if (StrToInt(dividend[u]) mod 2 <> 0) then
+      if (remem = true) then
         begin
-        if (remem = false) then
-          begin
-          remem:=true;
-          if (u = length(dividend)) then dividend:=concat(dividend, '1');
-          div_res:=concat(div_res, IntToStr(StrToInt(dividend[u]) div 2));
-          end
+        div_res:=div_res + IntToStr((10 + StrToInt(dividend[u])) div 2);
 
-        else begin
-          if (u <> length(dividend)) then
-            begin
-            div_res:=concat(div_res, IntToStr((10 + StrToInt(dividend[u])) div 2));
-            if ((10 + StrToInt(dividend[u])) mod 2 = 0) then remem:=false else remem:=true;
-            end
-
-          else begin
-            div_res:=concat(div_res, IntToStr((9 + StrToInt(dividend[u])) div 2));
-            if ((9 + StrToInt(dividend[u])) mod 2 = 0) then remem:=false else remem:=true;
-            end;
-          end;
+        if ((10 + StrToInt(dividend[u])) mod 2 = 1) then remem:=true else remem:=false;
         end
 
       else begin
-        if (remem = false) then div_res:=concat(div_res, IntToStr(StrToInt(dividend[u]) div 2))
+        remem:=true;
+        div_res:=div_res + IntToStr(StrToInt(dividend[u]) div 2);
 
-        else begin
-          if (u <> length(dividend)) then
-            begin
-            div_res:=concat(div_res, IntToStr((10 + StrToInt(dividend[u])) div 2));
-            if ((10 + StrToInt(dividend[u])) mod 2 = 0) then remem:=false else remem:=true;
-            end
-
-          else begin
-            div_res:=concat(div_res, IntToStr((9 + StrToInt(dividend[u])) div 2));
-            if ((9 + StrToInt(dividend[u])) mod 2 = 0) then remem:=false else remem:=true;
-            end;
-          end;
+        if (u = length(dividend)) then dividend:=dividend + '0';
         end;
-      end;
-
-    if (dividend[u] = '.') then div_res:=concat(div_res, '.');
-
-    if (u = length(dividend)) then
-      begin
-      if (pos('.', div_res) = 0) then
-        begin
-        insert('.', div_res, pos('0', div_res) + 1);
-        end;
-      end;
-    end;
-
-  if (s[i] <> '0') then
-    begin
-    dec_plus:=div_res;
-
-    if (length(dec_res) > 0) then
-      begin
-      dec_sum:=dec_res; dec_res:='';
-      end;
-
-    if (length(dec_plus) > length(dec_sum)) then
-      begin
-      dec_sum:=concat(dec_sum, dupestring('0', length(dec_plus) - length(dec_sum)));
       end
 
     else begin
-      dec_plus:=concat(dec_plus, dupestring('0', length(dec_sum) - length(dec_plus)));
+      if (remem = true) then
+        begin
+        remem:=false;
+        div_res:=div_res + IntToStr((10 + StrToInt(dividend[u])) div 2);
+        end
+
+      else begin
+        div_res:=div_res + IntToStr((StrToInt(dividend[u]) div 2));
+        end;
+      end;
+  until (u = length(dividend));
+
+  if (s[i] <> '0') then
+    begin
+    dec_res:='';
+    remem:=false;
+
+    if (length(div_res) > length(dec_sum)) then
+      begin
+      dec_sum:=dec_sum + dupestring('0', length(div_res) - length(dec_sum));
+      end
+
+    else begin
+      div_res:=div_res + dupestring('0', length(dec_sum) - length(div_res));
       end;
 
-    remem:=false;
-    for u:=length(dec_sum) downto 1 do
+    for u:=length(dec_sum) downto 2 do
       begin
-      if (dec_sum[u] <> '.') and (dec_plus[u] <> '.') then
+      if (div_res[u] <> '.') and (dec_sum[u] <> '.') then
         begin
-        if (StrToInt(dec_sum[u]) + StrToInt(dec_plus[u]) < 10) then
+        if (StrToInt(div_res[u]) + StrToInt(dec_sum[u]) < 10) then
           begin
           if (remem = true) then
             begin
-            if (StrToInt(dec_sum[u]) + StrToInt(dec_plus[u]) + 1 > 9) then
+            if (StrToInt(div_res[u]) + StrToInt(dec_sum[u]) + 1 = 10) then
               begin
-              dec_res:=concat(IntToStr((StrToInt(dec_sum[u]) + StrToInt(dec_plus[u]) + 1) mod 10), dec_res);
-              remem:=true
+              dec_res:='0' + dec_res;
               end
 
             else begin
-              dec_res:=concat(IntToStr(StrToInt(dec_sum[u]) + StrToInt(dec_plus[u]) + 1), dec_res);
               remem:=false;
+              dec_res:=IntToStr(StrToInt(div_res[u]) + StrToInt(dec_sum[u]) + 1) + dec_res;
               end;
             end
 
           else begin
-            dec_res:=concat(IntToStr(StrToInt(dec_sum[u]) + StrToInt(dec_plus[u])), dec_res);
+            dec_res:=IntToStr(StrToInt(div_res[u]) + StrToInt(dec_sum[u])) + dec_res;
             end;
           end
 
         else begin
           if (remem = true) then
             begin
-            if (StrToInt(dec_sum[u]) + StrToInt(dec_plus[u]) + 1 > 9) then
-              begin
-              dec_res:=concat(IntToStr((StrToInt(dec_sum[u]) + StrToInt(dec_plus[u]) + 1) mod 10), dec_res);
-              remem:=true
-              end
-
-            else begin
-              dec_res:=concat(IntToStr(StrToInt(dec_sum[u]) + StrToInt(dec_plus[u]) + 1), dec_res);
-              remem:=false;
-              end;
+            dec_res:=IntToStr((StrToInt(div_res[u]) + StrToInt(dec_sum[u]) + 1) mod 10) + dec_res
             end
 
           else begin
-            dec_res:=concat(IntToStr((StrToInt(dec_sum[u]) + StrToInt(dec_plus[u])) mod 10), dec_res);
+            dec_res:=IntToStr((StrToInt(div_res[u]) + StrToInt(dec_sum[u])) mod 10) + dec_res;
             end;
 
           remem:=true;
           end;
         end
 
-      else dec_res:=concat('.', dec_res);
+      else dec_res:='.' + dec_res;
       end;
+
+    dec_sum:='0' + dec_res;
     end;
   end;
-
-delete(dec_res, 1, 1);
 
 write(dec_res);
 end;
@@ -296,40 +257,39 @@ for i:=1 to sep - 1 do
       begin
       if (remem = true) then
         begin
-        num_res:=concat(IntToStr(StrToInt(num_mul[u]) * 2 + 1), num_res);
+        num_res:=IntToStr(StrToInt(num_mul[u]) * 2 + 1) + num_res;
         if (StrToInt(num_mul[u]) * 2 + 1 > 10) then remem:=true else remem:=false;
         end
 
-      else num_res:=concat(IntToStr(StrToInt(num_mul[u]) * 2), num_res);
+      else num_res:=IntToStr(StrToInt(num_mul[u]) * 2) + num_res;
       end
 
     else begin
       if (remem = true) then
         begin
-        num_res:=concat(IntToStr((StrToInt(num_mul[u]) * 2 + 1) mod 10), num_res);
+        num_res:=IntToStr((StrToInt(num_mul[u]) * 2 + 1) mod 10) + num_res;
         end
 
       else begin
-        num_res:=concat(IntToStr((StrToInt(num_mul[u]) * 2) mod 10), num_res);
+        num_res:=IntToStr((StrToInt(num_mul[u]) * 2) mod 10) + num_res;
         remem:=true;
         end;
       end;
 
-    if (u = 1) and (remem = true) then num_res:=concat('1', num_res);
+    if (u = 1) and (remem = true) then num_res:='1' + num_res;
     end;
 
   if (s[i] = '1') then
     begin
-    num_res:=concat(num_res, IntToStr(StrToInt(num_res[length(num_res)]) + 1));
-    delete(num_res, length(num_res) - 1, 1);
+    num_res:=IntToStr(StrToInt(num_res[length(num_res)]) + 1)[1];
     end;
 
   num_mul:=num_res;
 
-  if (i <> sep - 1) then num_res:='';
+  if (i < sep - 1) then num_res:='';
   end;
 
-if (negative = true) then num_res:=concat('-', num_res);
+if (negative = true) then num_res:='-' + num_res;
 
 write(num_res);
 
@@ -350,7 +310,7 @@ repeat
 
     if (key = '.') then decimal:=true;
 
-    s:=concat(s, key);
+    s:=s + key;
 
     write(key);
     end;
@@ -358,7 +318,7 @@ repeat
   if (GetAsyncKeyState(VK_CONTROL) < 0) and (GetAsyncKeyState(86) < 0) then
     begin
     i:=length(s);
-    s:=concat(s, PasteFromClip);
+    s:=s + PasteFromClip;
 
     write(Copy(s, i + 1, length(s)));
 
@@ -389,44 +349,31 @@ repeat
     delete(s, length(s), 1);
     end;
 
-  if (key = #13) and (length(s) <> 0) then
+  if (length(s) > 0) and (key = #13) then
     begin
     if (negative = true) then delete(s, 1, 1);
 
-    if (s[length(s)] = '.') then
+    if (decimal = true) and ((s[length(s)] = '0') or (s[length(s)] = '.')) then
       begin
-      decimal:=false;
-      delete(s, length(s), 1);
-      end;
-
-    if (s[1] = '.') then s:=concat('0', s);
-
-    if (length(s) > 1) and (decimal = true) and (s[length(s)] = '0') then
-      begin
-      i:=length(s) + 1;
       repeat
-        dec(i);
-        if (s[i] = '.') then decimal:=false;
-        delete(s, i, 1);
-      until (s[i - 1] = '1');
+        if (s[length(s)] = '.') then decimal:=false;
+
+        delete(s, length(s), 1);
+      until (s[length(s)] = '1') or (decimal = false);
       end;
 
-    if (length(s) > 1) and (s[1] = '0') and (s[2] <> '.') then
+    if (length(s) > 1) and ((s[1] = '0') or (s[1] = '.')) then
       begin
-      i:=1;
       repeat
-        delete(s, i, 1);
-      until (s[i] = '0') and (s[i + 1] = '.') or (s[i] = '1');
-      end;
+        if (s[1] = '.') then s:='0' + s
 
-    if (decimal = false) then sep:=length(s) + 1
+        else delete(s, 1, 1);
+       until (s = '0') or (s[1] = '1') or (s[2] = '.');
+       end;
 
-    else begin
-      for i:=1 to length(s) do
-        begin
-        if (s[i] = '.') then sep:=i;
-        end;
-      end;
+    if (decimal = true) then sep:=pos('.', s)
+
+    else sep:=length(s) + 1;
 
     writeln(#13#10#13#10'Convert to decimal: ');
     end;
